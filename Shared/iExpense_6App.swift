4b2e1b9cdfe67382e30e9e8a64e4322a30f8f5c0
Wade Belden <wade.belden1@gmail.com>
2021-04-07 09:50:04 -0500

    Initial Commit

---
+++ Shared/iExpense_6App.swift
@@ -0,0 +1,17 @@
+//
+//  iExpense_6App.swift
+//  Shared
+//
+//  Created by Wade Belden on 4/7/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpense_6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
